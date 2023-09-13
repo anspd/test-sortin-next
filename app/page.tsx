2906d10c8b6a8d4cdfc6df16968f4a575331b03b
Anshuman Padhi <anspd@live.in>
2023-09-13 22:19:51 +0530

    blahxeqw

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,20 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+
 import Link from "next/link";
 import LogoutButton from "../components/LogoutButton";
 import SupabaseLogo from "../components/SupabaseLogo";
 import NextJsLogo from "../components/NextJsLogo";
 import LandingPage from "@/pages/LandingPage";
-import BlogAdminPage from "@/pages/BlogAdminPage";
 import { usePathname } from "next/navigation";
 
 export default async function Index() {
-  const supabase = createServerComponentClient({ cookies });
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
+ 
 
   return (
     <div>
   
-        {user ? (
-          <BlogAdminPage/>
-        ) : (
+        
           <LandingPage/>
-        )}
+   
     
 
 
